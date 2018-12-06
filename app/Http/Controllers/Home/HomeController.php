ecd5f1115d90d6434a40477c8162034bbe73f726
Luciano Guerra <dsibr@hotmail.com>
2018-12-06 12:39:23 -0200

    Added counter values in home

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -2,13 +2,27 @@
 
 namespace CCZ\Http\Controllers\Home;
 
+use CCZ\Entities\Dog\Repository as Dog;
+use CCZ\Entities\Raca\Repository as Raca;
 use Illuminate\Http\Request;
 use CCZ\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-        return view('pages.home.home');
-    }
+  
+  private $dog;
+  private $raca;
+  
+  public function __construct(Dog $dog, Raca $raca)
+  {
+    $this->dog = $dog;
+    $this->raca = $raca;
+  }
+  
+  public function index()
+  {
+    $dogs = count($this->dog->all());
+    $racas = count($this->raca->all());
+    return view('pages.home.home', compact('dogs', 'racas'));
+  }
 }
--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -3,21 +3,39 @@
 @section('title', 'Categories')
 
 @section('content_header')
-<h1>Dashboard Settings
-  <small> Home</small>
-</h1>
+  <h1>Dashboard
+  </h1>
 @stop
 
 
 @section('content')
-<div class="box">
+  <div class="box">
 
+    <div class="box-body p-4">
+      <div class="row">
+        <div class="col-lg-3">
+          <div class="jumbotron text-center">
+            <i class="fa fa-dog fa-5x text-info"></i>
+            <div class="card-body">
+              <h5 class="card-title">Registro de cachorros</h5>
+              <span class="text-info" style="font-size: 7.0rem; font-weight: bolder; margin: 0">{{ $dogs }}</span> <br>
+              <a href="{{ route('dogs.index') }}" class="btn btn-info">Ver registros</a>
+            </div>
+          </div>
+        </div>
 
-  <div class="box-body">
-    <div class="jumbotron p-3">
-      home
-
+        <div class="col-lg-3">
+          <div class="jumbotron text-center">
+            <i class="fa fa-list-ul fa-5x text-primary"></i>
+            <div class="card-body">
+              <h5 class="card-title">Registro de raças</h5>
+              <span class="text-primary" style="font-size: 7.0rem; font-weight: bolder; margin: 0">{{ $racas }}</span>
+              <br>
+              <a href="{{ route('raca.index') }}" class="btn btn-primary">Ver raças</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 @stop
